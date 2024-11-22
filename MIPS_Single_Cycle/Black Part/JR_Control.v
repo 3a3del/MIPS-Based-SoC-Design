797748b82cad099f81e9cf5dece85a7bcd34da18
Kareem Atef <143643235+KareemAtefEECE@users.noreply.github.com>
2024-11-22 20:23:03 +0200

    Update JR_Control.v

--- MIPS_Single_Cycle/Black Part/JR_Control.v
+++ MIPS_Single_Cycle/Black Part/JR_Control.v
@@ -1,9 +1,22 @@
 
-module JR_Control(inst_in,JRControl);
+module JR_Control(inst_in,ALU_Op,JRControl);
 
-input[15:0];
+// What JR inst do is that it jumps to the inst. address stored in reg[rs].
+// Knowing that JR is an R-Type inst it uses the ALUOp as its a decoder diffrentiate 
+//between these (lw,sw,r-type,bne) for the alu to know if we have an R-Type instr and upon 
+
+/*
+ALU Signals:
+00->lw,sw
+01->beq
+10->r-type
+*/
+
+
+input[15:0] inst_in;
+input[1:0] ALU_Op;
 output JRControl;
 
-assign JRControl = (inst_in[5:0]==6'b001000)?1:0;
+assign JRControl = ((ALU_Op==2)&(inst_in[5:0]==6'b001000))?1:0;
 
 endmodule
